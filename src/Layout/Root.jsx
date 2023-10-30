882c6c3c4d444d810dfcc6594ae23e5b98a285a5
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-30 21:45:35 +0600

    footer file added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Home from '../Pages/Home';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div className="max-w-[1440px] mx-auto">
             <Home />
-
             <Outlet />
+            <Footer />
         </div>
     );
 };
