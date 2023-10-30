58636bae0df7da7275171b4f412e957978337320
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-31 01:13:32 +0600

    nav hover

--- src/NavBar/Banner.jsx
+++ src/NavBar/Banner.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import banner from "../assets/images/banner.png"
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
         <div className="bg-[#F5F7FA] mb-24 mt-10">
             <div className="flex md:px-28 md:py-14 justify-between flex-col md:flex-row px-6 py-14">
-                <div className='mb-10'>
-                    <h1 className="md:text-7xl font-bold mb-5 text-3xl md:text-left text-center">Lessons and insights <br /> <span className='text-[#4CAF4F]'>from 8 years</span></h1>
-                    <p className="text-center mb-6 md:text-left">Where to grow your business as a photographer: site or social media?</p>
+                <div className='mb-12'>
+                    <h1 className="md:text-7xl font-bold mb-5 text-3xl md:text-left text-center md:leading-tight">Lessons and insights <br /> <span className='text-[#4CAF4F]'>from 8 years</span></h1>
+                    <p className="text-center mb-7 mt-3 md:text-left">Where to grow your business as a photographer: site or social media?</p>
                     <div className='ml-24 md:ml-0'>
-                        <input className='bg-[#4CAF4F] w-[130px] h-[50px] left-28 mx-auto rounded-md' type="button" value="Register" />
+                        <Link>
+                            <input className='bg-[#4CAF4F] text-white font-semibold w-[130px] h-[50px] left-28 mx-auto hover:border border-[#4CAF4F] hover:bg-transparent cursor-pointer hover:text-[#4CAF4F] duration-500 rounded-md' type="button" value="Register" />
+                        </Link>
                     </div>
                 </div>
-                <img className="md:w-[390px] md:h-[390px] object-cover p-2" src={banner} alt="" />
+                <img className="md:w-[390px] md:h-[390px]  p-4" src={banner} alt="" />
             </div>
         </div>
     );
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ import logo from "../assets/logo.png"
 const NavBar = () => {
     const [toggle, setToggle] = useState()
     return (
-        <div className='bg-[#F5F7FA] p-5'>
+        <div className='bg-[#F5F7FA] md:px-28 py-4'>
             <div className="flex justify-between items-center">
                 <div>
                     <Link to="/"><img className="w-36" src={logo} alt="" /></Link>
@@ -25,7 +25,7 @@ const NavBar = () => {
 
 
                 <div className={`flex items-center duration-300 flex-col py-5 md:p-0 text-white md:text-gray-900 bg-opacity-90 top-14 bg-black md:bg-transparent w-full text-center md:w-auto md:flex-row gap-6 absolute md:static z-50 ${toggle ? "left-0" : "-left-full"}`}>
-                    <ul className="flex gap-6 flex-col md:flex-row">
+                    <ul className="flex flex-col md:flex-row gap-6">
                         <li>
                             <NavLink to="/">Home</NavLink>
                         </li>
@@ -47,10 +47,10 @@ const NavBar = () => {
                     </ul>
 
                     {/* LOGING AND SING UP BTN  */}
-                    
-                    <div className="flex gap-6 items-center font-semibold">
-                        <button className="border rounded-md p-2 border-[#4CAF4F]">Login</button>
-                        <button className="bg-[#4CAF4F] p-2 rounded-md text-white ">Sing Up</button>
+
+                    <div className="flex gap-2 items-center font-semibold md:ml-8">
+                        <button className='text-[#4CAF4F] px-5 py-2 font-bold duration-500 border-[#4CAF4F] hover:bg-[#4CAF4F] hover:text-white rounded-md'>Login</button>
+                        <button className='bg-[#4CAF4F] text-white px-5 py-2 font-bold duration-500 border-[#4CAF4F] hover:bg-transparent hover:text-[#4CAF4F] rounded-md'>Sing Up</button>
                     </div>
                 </div>
             </div>
