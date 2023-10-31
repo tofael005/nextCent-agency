85725007fed0d15908ff7e8791642eaf6d7dfe39
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-31 19:59:01 +0600

    smith added section

--- src/Pages/Smith.jsx
+++ src/Pages/Smith.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import image from "../assets/images/image00.png"
 
 const Smith = () => {
     return (
-        <div>
-            <h1>jfjfgj</h1>
+        <div className='md:px-28 mb-14 mt-10 bg-[#F5F7FA] px-3'>
+            <div>
+                <img src={image} alt="" />
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
