cbaa202141626e252e12407ae40370ccf3c7b9ae
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-01 22:04:58 +0600

    footer added

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from "../assets/foooter.png"
-import { BsInstagram, BsBrowserChrome, BsTwitter, BsYoutube } from 'react-icons/bs';
+import { BsInstagram, BsBrowserChrome, BsTwitter, BsYoutube, BsTelegram } from 'react-icons/bs';
 
 const Footer = () => {
     return (
@@ -18,18 +18,32 @@ const Footer = () => {
                 </div>
                 <div className="md:w-[600px] w-full">
                     <div className='grid md:grid-cols-3 gap-5'>
-                        <div className="text-center">
-                            <h1>Company</h1>
+                        <div className="text-center md:text-left">
+                            <h1 className='text-2xl font-semibold mb-2'>Company</h1>
+                            <p className='mb-1'>About</p>
+                            <p className='mb-1'>Blog</p>
+                            <p className='mb-1'>Contact us</p>
+                            <p className='mb-1'>Pricing</p>
+                            <p className='mb-1'>Testimonials</p>
                         </div>
                         <div>
-                            <h1>Support</h1>
+                            <h1 className='text-2xl font-semibold mb-2'>Support</h1>
+                            <p className='mb-1'>Help center</p>
+                            <p className='mb-1'>Terms of service</p>
+                            <p className='mb-1'>Legal</p>
+                            <p className='mb-1'>Privacy policy</p>
+                            <p className='mb-1'>Status</p>
                         </div>
-                        <div>
-                            <h1>Stay up to date</h1>
+                        <div className='text-center md:text-left'>
+                            <h1 className='text-2xl font-semibold mb-2'>Stay up to date</h1>
+                            <div className='flex cursor-pointer w-[200px] mx-auto'>
+                                <input className='p-4 rounded-md relative' type="search" name="" id="" placeholder='Your email address' />
+                                <BsTelegram className='text-black text-3xl absolute md:ml-44 ml-44 mt-3' />
+                            </div>
                         </div>
                     </div>
                 </div>
-               
+
             </div>
         </div>
     );
