2c47005f70c794334d7cc2152d846b08cbe0f06f
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-07 22:50:10 +0600

    text

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,17 +3,27 @@ import logo from "../assets/images/agency1.jpg"
 
 const About = () => {
     return (
-        <div>
+        <div className="px-3 md:px-0">
             <h1>About</h1>
-            <span className='flex flex-col md:flex-row px-3 md:px-0 gap-3'>
+            <span className='flex flex-col md:flex-row gap-3'>
                 <img className='md:w-[500px] object-cover' src={logo} alt="" />
                 <div>
-                    <p className="text-justify"><span className='font-bold'>Mission Statement:</span> At BioTech Connect, we are committed to leveraging cutting-edge digital solutions to empower the bio industry. Our mission is to bridge the gap between technology and biology, enabling our clients to innovate, discover, and advance their bio-related projects in an efficient and sustainable manner.</p>
-                    <p className="mt-2 text-justify"><span className='font-bold'>Bioinformatics Solutions: </span> Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.</p>
-                    <p className="mt-2 text-justify"><span className='font-bold'>Laboratory Information Management Systems (LIMS): </span> We develop and implement state-of-the-art LIMS software to streamline sample tracking, data management, and compliance for laboratories and research facilities. Our LIMS solutions are designed to enhance efficiency and data integrity.</p>
+                    <p className="md:text-justify"><span className='font-bold'>Mission Statement:</span> At BioTech Connect, we are committed to leveraging cutting-edge digital solutions to empower the bio industry. Our mission is to bridge the gap between technology and biology, enabling our clients to innovate, discover, and advance their bio-related projects in an efficient and sustainable manner.</p>
+                    <p className="mt-2 md:text-justify"><span className='font-bold'>Bioinformatics Solutions: </span> Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.</p>
+                    <p className="mt-2 md:text-justify"><span className='font-bold'>Laboratory Information Management Systems (LIMS): </span> We develop and implement state-of-the-art LIMS software to streamline sample tracking, data management, and compliance for laboratories and research facilities. Our LIMS solutions are designed to enhance efficiency and data integrity.</p>
+                    <p className="mt-2 md:text-justify"><span className='font-bold'>Bioprocess Optimization:</span>We offer digital solutions for bioprocess optimization, including modeling, simulation, and control systems. This helps bio-manufacturers improve yield, reduce costs, and minimize waste. We offer digital solutions for bioprocess optimization, including modeling, simulation, and control systems. This helps bio-manufacturers improve yield, reduce costs, and minimize waste.</p>
                 </div>
-
             </span>
+
+            <div>
+                
+                <p className="mt-2"><span className="font-bold">Team:</span>BioTech Connect is staffed with a diverse team of experts, including bioinformaticians, biologists, data scientists, software developers, project managers, and marketing specialists. Our interdisciplinary approach ensures that we understand the unique challenges and opportunities in the bio industry.BioTech Connect is your trusted partner for digital services in the bio industry. We look forward to helping you bridge technology and biology to achieve your goals and make a meaningful impact in the world of biotechnology.meaningful impact in the world of biotechnology.</p>
+            </div>
+
+            <div>
+                
+            </div>
+
         </div>
     );
 };
