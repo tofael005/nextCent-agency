15843ec35a1c31f4a73e5fa8b7c0ebca2b4c7230
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-08 00:08:10 +0600

    about imgaes update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,8 +3,7 @@ import logo from "../assets/images/agency1.jpg"
 
 const About = () => {
     return (
-        <div className="px-3 md:px-0">
-            <h1>About</h1>
+        <div className="px-3 md:px-0 mt-10">
             <span className='flex flex-col md:flex-row gap-3'>
                 <img className='md:w-[500px] object-cover' src={logo} alt="" />
                 <div>
@@ -19,8 +18,8 @@ const About = () => {
                 
                 <p className="mt-2"><span className="font-bold">Team:</span>BioTech Connect is staffed with a diverse team of experts, including bioinformaticians, biologists, data scientists, software developers, project managers, and marketing specialists. Our interdisciplinary approach ensures that we understand the unique challenges and opportunities in the bio industry.BioTech Connect is your trusted partner for digital services in the bio industry. We look forward to helping you bridge technology and biology to achieve your goals and make a meaningful impact in the world of biotechnology.meaningful impact in the world of biotechnology.</p>
             </div>
-                    <h1>Why Choose BioTech Connect:</h1>
-            <div className="grid md:grid-cols-2 gap-6">
+                    <h1 className="font-bold my-5">Why Choose BioTech Connect:</h1>
+            <div className="grid md:grid-cols-2 gap-6 mb-11">
                 <div>
                     <p><span className="font-bold">Specialized Expertise:</span> We are at the intersection of biology and technology, ensuring that our clients receive solutions that are tailored to their specific needs.</p>
                 </div>
@@ -31,7 +30,19 @@ const About = () => {
                     <p><span className="font-bold">Client-Centric Approach: </span>  We work closely with our clients to understand their goals and challenges, providing customized solutions that meet their objectives.</p>
                 </div>
                 <div>
-                    <p><span className="font-bold">Client-Centric Approach: </span>  We work closely with our clients to understand their goals and challenges, providing customized solutions that meet their objectives.</p>
+                    <p><span className="font-bold">Data Security: </span>  We prioritize the security and integrity of sensitive biological data, implementing robust cybersecurity measures and compliance standards.</p>
+                </div>
+                <div>
+                    <p><span className="font-bold">Effective Communication: </span> We believe in transparent and open communication to keep our clients informed throughout every project.</p>
+                </div>
+                <div>
+                    <p><span className="font-bold">Biotech Website and App Development:</span> We design and develop responsive websites and mobile applications for biotech companies, research institutions, and startups. Our digital solutions help clients communicate their scientific advancements, products, and services to a broader audience.</p>
+                </div>
+                <div>
+                    <p><span className="font-bold">Bio-Tech Marketing and Content Creation:</span> Our agency provides marketing and content creation services tailored to the bio industry. We create engaging content, manage social media, and design digital marketing campaigns to help clients promote their products.</p>
+                </div>
+                <div>
+                    <p><span className="font-bold">Bio-Tech Marketing and Content Creation:</span> Our agency provides marketing and content creation services tailored to the bio industry. We create engaging content, manage social media, and design digital marketing campaigns to help clients promote their products.</p>
                 </div>
             </div>
 
