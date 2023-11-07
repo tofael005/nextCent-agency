08bec4c763bfddec71bdc9b81a6fbbfa7061f950
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-07 21:46:29 +0600

    another text

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                 <div>
                     <p className="text-justify"><span className='font-bold'>Mission Statement:</span> At BioTech Connect, we are committed to leveraging cutting-edge digital solutions to empower the bio industry. Our mission is to bridge the gap between technology and biology, enabling our clients to innovate, discover, and advance their bio-related projects in an efficient and sustainable manner.</p>
                     <p className="mt-2 text-justify"><span className='font-bold'>Bioinformatics Solutions: </span> Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.</p>
-                    <p className="mt-2 text-justify"><span className='font-bold'>Bioinformatics Solutions: </span> Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.</p>
+                    <p className="mt-2 text-justify"><span className='font-bold'>Laboratory Information Management Systems (LIMS): </span> We develop and implement state-of-the-art LIMS software to streamline sample tracking, data management, and compliance for laboratories and research facilities. Our LIMS solutions are designed to enhance efficiency and data integrity.</p>
                 </div>
 
             </span>
