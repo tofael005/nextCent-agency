0884b0b2a19e124c9c21dcb33348b570fe28b07c
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-07 21:44:38 +0600

    about text added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,49 +5,15 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            <p>Mission Statement: At BioTech Connect, we are committed to leveraging cutting-edge digital solutions to empower the bio industry. Our mission is to bridge the gap between technology and biology, enabling our clients to innovate, discover, and advance their bio-related projects in an efficient and sustainable manner.
+            <span className='flex flex-col md:flex-row px-3 md:px-0 gap-3'>
+                <img className='md:w-[500px] object-cover' src={logo} alt="" />
+                <div>
+                    <p className="text-justify"><span className='font-bold'>Mission Statement:</span> At BioTech Connect, we are committed to leveraging cutting-edge digital solutions to empower the bio industry. Our mission is to bridge the gap between technology and biology, enabling our clients to innovate, discover, and advance their bio-related projects in an efficient and sustainable manner.</p>
+                    <p className="mt-2 text-justify"><span className='font-bold'>Bioinformatics Solutions: </span> Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.</p>
+                    <p className="mt-2 text-justify"><span className='font-bold'>Bioinformatics Solutions: </span> Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.</p>
+                </div>
 
-                Services Offered: <span><img src={logo} alt="" /></span>
-
-                1. Bioinformatics Solutions: Our team of bioinformaticians and data scientists provides custom data analysis, interpretation, and visualization services. We assist in genomics, proteomics, and metabolomics data analysis, helping researchers and biotech companies make data-driven decisions.
-
-                2. Laboratory Information Management Systems (LIMS): We develop and implement state-of-the-art LIMS software to streamline sample tracking, data management, and compliance for laboratories and research facilities. Our LIMS solutions are designed to enhance efficiency and data integrity.
-
-                3. Bioprocess Optimization: We offer digital solutions for bioprocess optimization, including modeling, simulation, and control systems. This helps bio-manufacturers improve yield, reduce costs, and minimize waste.
-
-                4. Biosecurity and Compliance Services: BioTech Connect provides support in ensuring compliance with regulatory standards and securing sensitive bio-related data. Our cybersecurity and compliance services help clients safeguard their research and maintain data integrity.
-
-                5. Bio-Data Visualization: We specialize in creating interactive and informative data visualizations to help researchers and organizations present their findings effectively and make complex biological data more understandable.
-
-                6. Biotech Website and App Development: We design and develop responsive websites and mobile applications for biotech companies, research institutions, and startups. Our digital solutions help clients communicate their scientific advancements, products, and services to a broader audience.
-
-                7. Bio-Tech Marketing and Content Creation: Our agency provides marketing and content creation services tailored to the bio industry. We create engaging content, manage social media, and design digital marketing campaigns to help clients promote their products, services, and research projects.
-
-                Team:
-
-                8. BioTech Connect is staffed with a diverse team of experts, including bioinformaticians, biologists, data scientists, software developers, project managers, and marketing specialists. Our interdisciplinary approach ensures that we understand the unique challenges and opportunities in the bio industry.
-
-
-
-                Why Choose BioTech Connect:
-
-                1. Specialized Expertise: We are at the intersection of biology and technology, ensuring that our clients receive solutions that are tailored to their specific needs.
-
-                2. Innovation: We stay up-to-date with the latest advancements in the bio industry and technology, allowing us to offer cutting-edge services to our clients.
-
-                3. Client-Centric Approach: We work closely with our clients to understand their goals and challenges, providing customized solutions that meet their objectives.
-
-                4. Data Security: We prioritize the security and integrity of sensitive biological data, implementing robust cybersecurity measures and compliance standards.
-
-                Effective Communication: We believe in transparent and open communication to keep our clients informed throughout every project.
-
-                5. BioTech Connect is your trusted partner for digital services in the bio industry. We look forward to helping you bridge technology and biology to achieve your goals and make a meaningful impact in the world of biotechnology.
-                5. BioTech Connect is your trusted partner for digital services in the bio industry. We look forward to helping you bridge technology and biology to achieve your goals and make a meaningful impact in the world of biotechnology.
-
-
-
-
-            </p>
+            </span>
         </div>
     );
 };
