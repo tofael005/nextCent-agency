91345b024736596af186938503cb0e5c514ca21d
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-09 23:27:57 +0600

    service section

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Service = () => {
     return (
         <div>
-           <h1>Service Comming soon</h1>
+            <div>
+                <div>
+                    <h1>Creating Streamlined Safeguarding Processes with OneRen</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo rem necessitatibus ut fugiat? Dolore laborum fugit repellendus explicabo dolorem in commodi, adipisci, fugiat quisquam debitis laudantium! Mollitia sed voluptatem id iusto quia? Natus eos at error doloremque eligendi, nam porro ex nostrum, deserunt consectetur sequi laudantium animi! Adipisci, reprehenderit culpa.</p>
+                </div>
+            </div>
         </div>
     );
 };
