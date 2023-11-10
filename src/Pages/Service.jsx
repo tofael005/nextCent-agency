1d822e6f7f6b2cecd036fe35ba5596c8ebf76d66
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-10 22:43:07 +0600

    tag heading service

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -5,11 +5,11 @@ const Service = () => {
     return (
         <div className='mt-10 px-3'>
             <div className="grid md:grid-cols-4 gap-4">
-                <div>
+                <div className='border'>
                     <img className='rounded-md hover:shadow-md duration-300 hover:shadow-black/50 scale-100 hover:scale-95' src={iamge1} alt="" />
-                    <div className="flex items-center justify-center gap-2">
-                        <h1>Digital Marketing</h1>
-                        <p>1.5 month</p>
+                    <div className="flex items-center justify-between my-2 gap-2 ">
+                        <h1 className='font-semibold'>Digital Marketing</h1>
+                        <p className='text-gray-500'>1.5 month</p>
                     </div>
                 </div>
             </div>
