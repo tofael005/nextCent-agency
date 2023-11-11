20085b0c49f9fef6a4e87f2211e7e9b5622fc6d4
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-11 23:21:53 +0600

    servicess

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -3,14 +3,13 @@ import iamge1 from "../assets/service/service1.jpg"
 
 const Service = () => {
     return (
-        <div className='mt-10 px-3'>
+        <div className='mt-10 px-3 max-w-[1440px] mx-auto'>
             <div className="grid md:grid-cols-4 gap-4">
-                <div className='border'>
-                    <img className='rounded-md hover:shadow-md duration-300 hover:shadow-black/50 scale-100 hover:scale-95' src={iamge1} alt="" />
-                    <div className="flex items-center justify-between my-2 gap-2 ">
-                        <h1 className='font-semibold'>Digital Marketing</h1>
-                        <p className='text-gray-500'>1.5 month</p>
-                    </div>
+                <div className='rounded-md text-transparent hover:text-white hover:bg-black hover:shadow-md duration-300 cursor-pointer'>
+                    <img className='rounded-md relative' src={iamge1} alt="" />
+
+                    <h1 className='font-bold text-2xl  absolute md:top-[250px] md:left-[320px] bg-black bg-transparent'>Digital Marketing</h1>
+
                 </div>
             </div>
         </div>
