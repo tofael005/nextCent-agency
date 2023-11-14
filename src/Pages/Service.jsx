6382241e16a36f28349853ccac390ae0e2d235a9
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-14 23:05:13 +0600

    text some

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -5,11 +5,13 @@ const Service = () => {
     return (
         <div className='mt-10 px-3 max-w-[1440px] mx-auto'>
             <div className="grid md:grid-cols-4 gap-4">
-                <div className='rounded-md text-transparent hover:text-white hover:bg-black hover:shadow-md duration-300 cursor-pointer'>
-                    <img className='rounded-md relative' src={iamge1} alt="" />
+                <div className='rounded-md hover:shadow-md duration-300 cursor-pointer'>
+                    <img className='rounded-md' src={iamge1} alt="" />
 
-                    <h1 className='font-bold text-2xl  absolute md:top-[250px] md:left-[320px] bg-black bg-transparent'>Digital Marketing</h1>
-
+                    <div className='flex justify-between items-center'>
+                        <h1 className='font-bold text-2xl'>Digital Marketing</h1>
+                        <p>Details</p>
+                    </div>
                 </div>
             </div>
         </div>
