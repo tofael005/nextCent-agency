6050d1efafa66efd43aadf674178df27e48cdfd1
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-14 23:46:12 +0600

    servic another added

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -5,12 +5,12 @@ const Service = () => {
     return (
         <div className='mt-10 px-3 max-w-[1440px] mx-auto'>
             <div className="grid md:grid-cols-4 gap-4">
-                <div className='rounded-md hover:shadow-md duration-300 cursor-pointer'>
+                <div className='rounded-md hover:shadow-md duration-300 text-transparent hover:text-black cursor-pointer p-2'>
                     <img className='rounded-md' src={iamge1} alt="" />
 
-                    <div className='flex justify-between items-center'>
-                        <h1 className='font-bold text-2xl'>Digital Marketing</h1>
-                        <p>Details</p>
+                    <div className='flex justify-between items-center mt-3 '>
+                        <h1 className='font-bold text-xl'>Digital Marketing</h1>
+                        <p className='hover:text-blue-600 font-semibold'>Details</p>
                     </div>
                 </div>
             </div>
