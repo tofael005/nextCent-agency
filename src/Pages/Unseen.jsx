83f2da9b7c8f10f5f52f5e5faee38564607d1e51
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-18 21:27:09 +0600

    unseen app setup

--- src/Pages/Unseen.jsx
+++ src/Pages/Unseen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from "../assets/images/frame.png"
+import { Link } from 'react-router-dom';
 
 const Unseen = () => {
     return (
@@ -9,7 +10,7 @@ const Unseen = () => {
                 <div>
                     <h1 className="md:text-4xl text-2xl font-semibold leading-tight mb-3">The unseen of spending three years at Pixelgrade</h1>
                     <p className='text-justify mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
-                    <input className='bg-[#4CAF4F] text-white font-semibold w-[130px] h-[50px] md:ml-0 ml-24 hover:border border-[#4CAF4F] hover:bg-transparent cursor-pointer hover:text-[#4CAF4F] duration-500 rounded-md' type="button" value="Learn more" />
+                    <Link to="/about"><input className='bg-[#4CAF4F] text-white font-semibold w-[130px] h-[50px] md:ml-0 ml-24 hover:border border-[#4CAF4F] hover:bg-transparent cursor-pointer hover:text-[#4CAF4F] duration-500 rounded-md' type="button" value="Learn more" /></Link>
                 </div>
             </div>
         </div>
